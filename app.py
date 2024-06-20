4e95eec07b3526f4bbcdb722158bc68316fc0607
Jonathan Gal <jonathan.gal@csf.technion.ac.il>
2024-06-20 14:12:32 +0300

    register and login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,3 +8,16 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.args.get('username')
+
+    return 'Hello, World!'
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    #
+    return username
