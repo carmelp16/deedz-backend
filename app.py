b6acbc512dabd5c8df51a4c1e55bac35263e9ccb
OBL <obl@example.com>
2024-06-21 01:16:02 +0300

    edit route test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from task_similarity import SemanticSim
 
 app = Flask(__name__)
 CORS(app)
@@ -7,4 +8,11 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    semantic_sim = SemanticSim()
+# # For comparing a single pair of strings
+    a = semantic_sim.calc_cosine_sim("Cooking", "I need help with preparing soup")
+# result: 0.38
+    b = semantic_sim.calc_cosine_sim("Cooking", "I need help with walking the dog")
+
+    return a + ', ' + b
+# result: 0.06
